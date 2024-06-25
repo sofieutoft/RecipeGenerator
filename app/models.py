ed5a60102f7c31b7a7ff46d7b53976694e8cca44
sofieutoft <sofieu26@gmail.com>
2024-06-25 13:50:17 -0500

    Update

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    cuisine = db.Column(db.String(50), nullable=False)
-    ingredients = db.Column(db.Text, nullable=False)
-    instructions = db.Column(db.Text, nullable=False)
