c7fe22d5295537b60a31175ae85fb2cd7dd80033
sofieutoft <sofieu26@gmail.com>
2024-06-25 12:38:05 -0500

    Setting up project

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    cuisine TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    instrucitons TEXT NOT NULL
+);
