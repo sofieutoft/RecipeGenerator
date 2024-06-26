ef220908aec9af610783414934db2dad397c5976
Sofia Utoft <126016299+sofieutoft@users.noreply.github.com>
2024-06-26 13:38:47 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 # Create table
 CREATE TABLE recipes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
+    meal TEXT NOT NULL,
     cuisine TEXT NOT NULL,
     ingredients TEXT NOT NULL,
     instructions TEXT NOT NULL
