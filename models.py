4c903b2c38f316708062e7f8c62c8b8b43701c5d
Sofia Utoft <126016299+sofieutoft@users.noreply.github.com>
2024-06-27 13:57:29 -0500

    Update models.py

--- models.py
+++ models.py
@@ -5,8 +5,9 @@ db = SQLAlchemy()
 
 # Create a class for each table in the database
 class Recipe(db.Model):
-    meal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     meal_name = db.Column(db.String(50), nullable=False)
+    meal_id = db.Column(db.Integer, nullable=False)
     cuisine = db.Column(db.String(50), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)
